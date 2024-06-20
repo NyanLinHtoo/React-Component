49b935604b8f272cfdbb7a0df8e7b780e32c8fce
NyanLinHtoo <nyanlinhtoo.ygn@gmail.com>
2024-06-20 19:36:31 +0700

    add Dropdown

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,8 @@ import Login from "./components/Login/Login";
 import ForgotPassword from "./components/ForgotPassword/ForgotPassword";
 import SignUp from "./components/SignUp/SignUp";
 import ResetPassword from "./components/ResetPassword/ResetPassword";
+import NavBar from "./components/NavBar/NavBar";
+import Dropdown from "./components/Dropdown/Dropdown";
 
 const App = () => {
   return (
@@ -10,6 +12,8 @@ const App = () => {
       {/* <SignUp /> */}
       {/* <ForgotPassword /> */}
       {/* <ResetPassword /> */}
+      {/* <NavBar /> */}
+      <Dropdown />
     </div>
   );
 };
